8d736a1bcc4925beddf012c8a4b64fb39f71a52a
LucasGabSil <136385159+LucasGabSil@users.noreply.github.com>
2023-08-06 12:00:10 -0300

    criando sistema de teste de multas

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 6
+
+if (vel <= 60){
+    console.log(`a velociade de carro é de ${vel} km/h. Voce esta dentro do limite da via`)
+} else {
+    console.log(`a velociade de carro é de ${vel} km/h. Voce ultrapassou a velocidade permitida da via e foi multado`)
+}
+
