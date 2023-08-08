25586920f4b94153c806a7b99c53f54a4b864c5c
LucasGabSil <136385159+LucasGabSil@users.noreply.github.com>
2023-08-08 18:31:32 -0300

    Adicionando exercícios finais do curso de JavaScript

---
+++ aula015/ambiente.js
@@ -0,0 +1,6 @@
+let num = [2, 8, 2, 9, 3]
+num.push(7)
+
+
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O vetor em ordem fica ${num.sort()}`)
---
+++ aula015/vetornatela.js
@@ -0,0 +1,5 @@
+let valores = [8,6,34,5,5,6,2,1]
+
+for (let pos=0; pos < valores.length; pos++){
+    console.log (`A posição ${pos} tem o valor ${valores[pos]}`)
+}
